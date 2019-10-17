7e20fed6a46c4c1aba222131b68bba2d33f2d83b
andrzej musiol <bigeyedes@gmail.com>
2019-10-17 19:57:14 +0200

    Basic files & configuration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
+# imports
 from flask import Flask
+
+# zmienna aplikacji
 app = Flask(__name__)
 
+# routing
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1>Flask jest spoko!</h1>'
 
-
+# debug
 if __name__=='__main__':
 	app.run(debug=True)
